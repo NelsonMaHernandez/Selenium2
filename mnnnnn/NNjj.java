f815e0a349a310f1872f2e0bf8e0572350807a4f
NelsonMaHernandez <nelmaher@gmail.com>
2022-08-27 12:58:27 -0400

    n

---
+++ mnnnnn/NNjj.java
@@ -0,0 +1,32 @@
+package mnnnnn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NNjj {
+
+	public static void main(String[] args) {
+
+	
+		int[] max = {23,24,56,78,90,67};
+		
+		List<Integer> maxNun = new ArrayList<>();
+		
+		for(int i =0; i< max.length; i ++) {
+			
+			maxNun.add(max[i]);
+			
+		}
+		System.out.println("This is the largest number " + Collections.max(maxNun));
+		
+
+	}
+	
+
+}
+		
+	
+
+
+
